1711ff0583411d257b16b708748419db394b9004
John Richard Chipps-Harding <mrharding@gmail.com>
2024-02-07 11:53:52 +0000

    Film Grain

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # comfyui-propost
+
+## Film Grain
+Based on this codebase:
+https://github.com/larspontoppidan/filmgrainer/tree/main
--- nodes.py
+++ nodes.py
@@ -58,15 +58,140 @@ class ProPostVignette:
         vignette = 1 - radius * opacity
 
         return np.clip(image * vignette[..., np.newaxis], 0, 1)
+
+
+
+
+
+        # self.gray_scale = False
+        # self.grain_type = 1
+        # self.grain_sat = 0.5        
+        # self.grain_power = 0.7
+        # self.shadows = 0.2
+        # self.highs = 0.2
+        # self.scale = 1.0
+        # self.sharpen = 0
+        # self.src_gamma = 1.0
+        # self.seed = 1
+        # self.file_in = None
+        # self.file_out = None
+
+class ProPostFilmGrain:
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "gray_scale": ("BOOL", {
+                    "default": False
+                }),
+                "grain_type": ("INT", {
+                    "default": 1,
+                    "min": 1,
+                    "max": 4
+                }),
+                "grain_sat": ("FLOAT", {
+                    "default": 0.5,
+                    "min": 0.0,
+                    "max": 1.0,
+                    "step": 0.01
+                }),
+                "grain_power": ("FLOAT", {
+                    "default": 0.7,
+                    "min": 0.0,
+                    "max": 1.0,
+                    "step": 0.01
+                }),
+                "shadows": ("FLOAT", {
+                    "default": 0.2,
+                    "min": 0.0,
+                    "max": 1.0,
+                    "step": 0.01
+                }),
+                "highs": ("FLOAT", {
+                    "default": 0.2,
+                    "min": 0.0,
+                    "max": 1.0,
+                    "step": 0.01
+                }),
+                "scale": ("FLOAT", {
+                    "default": 1.0,
+                    "min": 0.0,
+                    "max": 10.0,
+                    "step": 0.01
+                }),
+                "sharpen": ("INT", {
+                    "default": 0,
+                    "min": 0,
+                    "max": 10
+                }),
+                "src_gamma": ("FLOAT", {
+                    "default": 1.0,
+                    "min": 0.0,
+                    "max": 10.0,
+                    "step": 0.01
+                }),
+                "seed": ("INT", {
+                    "default": 1,
+                    "min": 1,
+                    "max": 1000
+                }),
+            },
+        }
+ 
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ()
+ 
+    FUNCTION = "filmgrain_image"
+ 
+    #OUTPUT_NODE = False
+ 
+    CATEGORY = "propost/Film Grain"
+ 
+    def filmgrain_image(self, image: torch.Tensor, gray_scale: bool, grain_type: int, grain_sat: float, grain_power: float, shadows: float, highs: float, scale: float, sharpen: int, src_gamma: float, seed: int):
+        batch_size, height, width, _ = image.shape
+        result = torch.zeros_like(image)
+
+        for b in range(batch_size):
+            tensor_image = image[b].numpy()
+
+            # Apply vignette
+            vignette_image = self.apply_vignette(tensor_image, 1.0)
+
+            tensor = torch.from_numpy(vignette_image).unsqueeze(0)
+            result[b] = tensor
+
+        return (result,)
+
+    def apply_filmgrain(self, image, gray_scale, grain_type, grain_sat, grain_power, shadows, highs, scale, sharpen, src_gamma, seed):
+        if grain_sat == 0:
+            return image
+
+        height, width, _ = image.shape
+        x = np.linspace(-1, 1, width)
+        y = np.linspace(-1, 1, height)
+        X, Y = np.meshgrid(x, y)
+        radius = np.sqrt(X ** 2 + Y ** 2)
+
+        radius = radius / np.max(radius)
+        opacity = np.clip(grain_sat, 0, 1)
+        vignette = 1 - radius * opacity
+
+        return np.clip(image * vignette[..., np.newaxis], 0, 1)
  
  
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
-    "ProPostVignette": ProPostVignette
+    "ProPostVignette": ProPostVignette,
+    "ProPostFilmGrain": ProPostFilmGrain
 }
  
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "ProPostVignette": "ProPost Vignette"
+    "ProPostVignette": "ProPost Vignette",
+    "ProPostFilmGrain": "ProPost Film Grain"
 }
