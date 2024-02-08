dec0b7a69bc0b9cd925f12a5dc483d1693820014
John Richard Chipps-Harding <mrharding@gmail.com>
2024-02-08 17:04:41 +0000

    Film Grain (#2)
    * Film Grain
    
    * Working!

--- nodes.py
+++ nodes.py
@@ -1,5 +1,15 @@
+import sys
+import os
 import torch
 import numpy as np
+
+# Get the directory of the current file
+current_file_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Append this directory to sys.path
+sys.path.append(current_file_directory)
+
+import filmgrainer.filmgrainer as filmgrainer
 
 class ProPostVignette:
     def __init__(self):
@@ -58,15 +68,115 @@ class ProPostVignette:
         vignette = 1 - radius * opacity
 
         return np.clip(image * vignette[..., np.newaxis], 0, 1)
+
+class ProPostFilmGrain:
+    grain_types = ["Fine", "Fine Simple", "Coarse", "Coarser"]
+
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "gray_scale": ("BOOLEAN", {
+                    "default": False
+                }),
+                "grain_type": (s.grain_types,),
+                "grain_sat": ("FLOAT", {
+                    "default": 0.5,
+                    "min": 0.0,
+                    "max": 1.0,
+                    "step": 0.01
+                }),
+                "grain_power": ("FLOAT", {
+                    "default": 0.7,
+                    "min": 0.0,
+                    "max": 1.0,
+                    "step": 0.01
+                }),
+                "shadows": ("FLOAT", {
+                    "default": 0.2,
+                    "min": 0.0,
+                    "max": 1.0,
+                    "step": 0.01
+                }),
+                "highs": ("FLOAT", {
+                    "default": 0.2,
+                    "min": 0.0,
+                    "max": 1.0,
+                    "step": 0.01
+                }),
+                "scale": ("FLOAT", {
+                    "default": 1.0,
+                    "min": 0.0,
+                    "max": 10.0,
+                    "step": 0.01
+                }),
+                "sharpen": ("INT", {
+                    "default": 0,
+                    "min": 0,
+                    "max": 10
+                }),
+                "src_gamma": ("FLOAT", {
+                    "default": 1.0,
+                    "min": 0.0,
+                    "max": 10.0,
+                    "step": 0.01
+                }),
+                "seed": ("INT", {
+                    "default": 1,
+                    "min": 1,
+                    "max": 1000
+                }),
+            },
+        }
+ 
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ()
+ 
+    FUNCTION = "filmgrain_image"
+ 
+    #OUTPUT_NODE = False
+ 
+    CATEGORY = "propost/Film Grain"
+ 
+    def filmgrain_image(self, image: torch.Tensor, gray_scale: bool, grain_type: str, grain_sat: float, grain_power: float, shadows: float, highs: float, scale: float, sharpen: int, src_gamma: float, seed: int):
+        batch_size, height, width, _ = image.shape
+        result = torch.zeros_like(image)
+
+        # find index of grain_type
+        grain_type_index = self.grain_types.index(grain_type) + 1;
+
+
+        for b in range(batch_size):
+            tensor_image = image[b].numpy()
+
+            # Apply vignette
+            vignette_image = self.apply_filmgrain(tensor_image, gray_scale, grain_type_index, grain_sat, grain_power, shadows, highs, scale, sharpen, src_gamma, seed)
+
+            tensor = torch.from_numpy(vignette_image).unsqueeze(0)
+            result[b] = tensor
+
+        return (result,)
+
+    def apply_filmgrain(self, image, gray_scale, grain_type, grain_sat, grain_power, shadows, highs, scale, sharpen, src_gamma, seed):
+        out_image = filmgrainer.process(image, scale, src_gamma, 
+            grain_power, shadows, highs, grain_type, 
+            grain_sat, gray_scale, sharpen, seed)
+        return out_image
  
  
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
-    "ProPostVignette": ProPostVignette
+    "ProPostVignette": ProPostVignette,
+    "ProPostFilmGrain": ProPostFilmGrain
 }
  
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "ProPostVignette": "ProPost Vignette"
+    "ProPostVignette": "ProPost Vignette",
+    "ProPostFilmGrain": "ProPost Film Grain"
 }
