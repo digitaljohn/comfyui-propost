d08755f83008cce2487112f2ea80e4115e6c9d28
John Richard Chipps-Harding <mrharding@gmail.com>
2024-02-09 15:55:09 +0000

    Refactor loading

--- nodes.py
+++ nodes.py
@@ -4,8 +4,8 @@ import cv2
 import torch
 import numpy as np
 import folder_paths
-from colour.io.luts.iridas_cube import read_LUT_IridasCube, LUT3D, LUT3x1D
 from .utils import processing as processing_utils
+from .utils import loading as loading_utils
 
 # Get the directory of the current file and add it to the system path
 current_file_directory = os.path.dirname(os.path.abspath(__file__))
@@ -389,7 +389,7 @@ class ProPostApplyLUT:
 
         # Read the LUT
         lut_path = os.path.join(dir_luts, lut_name)
-        lut = self.read_lut(lut_path, clip=True)
+        lut = loading_utils.read_lut(lut_path, clip=True)
 
         # Apply the LUT
         is_non_default_domain = not np.array_equal(lut.domain, np.array([[0., 0., 0.], [1., 1., 1.]]))
@@ -414,30 +414,6 @@ class ProPostApplyLUT:
         blended_image = (1 - strength) * image + strength * im_array
 
         return blended_image
-
-    def read_lut(self, lut_path, clip=False):
-        """
-        Reads a LUT from the specified path, returning instance of LUT3D or LUT3x1D
-
-        <lut_path>: the path to the file from which to read the LUT (
-        <clip>: flag indicating whether to apply clipping of LUT values, limiting all values to the domain's lower and
-            upper bounds
-        """
-        lut: Union[LUT3x1D, LUT3D] = read_LUT_IridasCube(lut_path)
-        lut.name = os.path.splitext(os.path.basename(lut_path))[0]  # use base filename instead of internal LUT name
-
-        if clip:
-            if lut.domain[0].max() == lut.domain[0].min() and lut.domain[1].max() == lut.domain[1].min():
-                lut.table = np.clip(lut.table, lut.domain[0, 0], lut.domain[1, 0])
-            else:
-                if len(lut.table.shape) == 2:  # 3x1D
-                    for dim in range(3):
-                        lut.table[:, dim] = np.clip(lut.table[:, dim], lut.domain[0, dim], lut.domain[1, dim])
-                else:  # 3D
-                    for dim in range(3):
-                        lut.table[:, :, :, dim] = np.clip(lut.table[:, :, :, dim], lut.domain[0, dim], lut.domain[1, dim])
-
-        return lut
  
  
 # A dictionary that contains all nodes you want to export with their names
